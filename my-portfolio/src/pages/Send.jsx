66d08dd537a0b10d8835b1c3eeeada3d11cac083
Jose Luis <jose.lmy2004@gmail.com>
2023-08-14 15:31:30 -0500

    NEW PROJECT

---
+++ my-portfolio/src/pages/Send.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../Styles/Send.css"
+
+function Send() {
+    return(
+        <>
+            <div className='send'>
+                <p>MENSAJE ENVIADO</p>
+            </div>
+        </>
+    )
+}
+
+export {Send}
